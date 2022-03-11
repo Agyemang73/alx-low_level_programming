caa4be02423e98c665aa69c779f0454deb52efa9
Agyemang73 <owusukevin17@gmail.com>
2022-03-11 13:28:39 -0800

    Alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-#include <ctype.h>
-
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Retrun: Always 0 (Success)
  */
-
 int main(void)
 {
-	int 1 = 'a';
-	while (1 <= 'z')
+	char c = 'a';
+
+	while (c <= 'z')
 	{
-		putchar(1);
-		l += 1;
+		putchar(c);
+		c++;
 	}
 	putchar('\n');
 	return (0);
