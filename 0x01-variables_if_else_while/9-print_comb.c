53318807f2b86df2b68fa8f5c904ea1e8b8f2f1c
Agyemang73 <owusukevin17@gmail.com>
2022-03-11 15:05:07 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints all possible comn-binations of single digit numbers.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i ='0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		if  (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		++i;
+	}
+	putchar('\n');
+	return (0);
+}
