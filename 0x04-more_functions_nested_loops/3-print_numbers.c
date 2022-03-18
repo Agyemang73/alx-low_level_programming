851902da9674812ee6f6663d2acc043abd6c45fc
Agyemang73 <owusukevin17@gmail.com>
2022-03-18 23:05:43 +0000

    Correction

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,14 @@
 #include "main.h"
-
 /**
  * print_numbers - function
- *Return: Always 0
  */
 void print_numbers(void)
 {
-	int i;
-	while (i < 10)
-	{
-		_putchar(i + '0');
-		i++;
-	}
+	int n = 48;
 
-	_putchar('\n');
+	while (n < 58)
+	{
+		_putchar(n++);
+	}
+	_putchar(10);
 }
