b1402d1bc15beeed2bba22a1930a1e08b8bd9454
Agyemang73 <owusukevin17@gmail.com>
2022-03-28 21:36:30 +0000

    corrections

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,7 +15,7 @@ unsigned int _strspn(char *s, char *accept)
 	for (x = 0; *(s + x) != 0; x++)
 	{
 		k = 0;
-		for (y = 0;  *(accept + y) != 0; y++)
+		for (y = 0; *(accept + y) != 0; y++)
 		{
 			if (*(s + x) == *(accept + y))
 			{
