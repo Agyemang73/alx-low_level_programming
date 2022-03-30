d0101d3cd635624ff62b4b66bf396df9656cd66b
Agyemang73 <owusukevin17@gmail.com>
2022-03-31 00:38:39 +0100

    Correction

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -24,13 +24,32 @@ int find_strlen(char *s)
 }
 
 /**
- * check_palindrome - checks if a string is a palindrome.
+ * check_palindrome - Checks if a string is a palindrome.
  * @s: The string to be checked.
  * @len: The length of s.
  * @index: The index of the string to be checked.
  *
- * Return: If the string is a palindrome - 1 not palindrome - 0
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
  */
+int check_palindrome(char *s, int len, int index)
+{
+	if (s[index] == s[len / 2])
+		return (1);
+
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
+
+	return (0);
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome.
+ * @s: The string to be checked.
+ * 
+ * Return: If the string is a palindrome - 1
+ *         If the string is not a palindrome - 0.
+ */         
 int is_palindrome(char *s)
 {
 	int index = 0;
