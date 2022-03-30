207e935cfd087667a43291b04f9a40f90d483b88
Agyemang73 <owusukevin17@gmail.com>
2022-03-31 00:41:13 +0100

    Correction to palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -46,10 +46,10 @@ int check_palindrome(char *s, int len, int index)
 /**
  * is_palindrome - checks if a string is a palindrome.
  * @s: The string to be checked.
- * 
+ *
  * Return: If the string is a palindrome - 1
  *         If the string is not a palindrome - 0.
- */         
+ */
 int is_palindrome(char *s)
 {
 	int index = 0;
