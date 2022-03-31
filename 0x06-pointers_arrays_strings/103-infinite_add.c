1824f19e03fce08c5b7c5b8fe58d5d7c1bd0b29a
Agyemang73 <owusukevin17@gmail.com>
2022-03-31 22:42:32 +0100

    correction

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -74,8 +74,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
 		return (0);
 
-	n1 += n1_len -1;
-	n2 += n2_len -1;
+	n1 += n1_len - 1;
+	n2 += n2_len - 1;
 	*(r + size_r) = '\0';
 
 	return (add_strings(n1, n2, r, --size_r));
