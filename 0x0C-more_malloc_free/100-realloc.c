491f42ca323176c96b1118cce374dc8346ad8921
Agyemamg73 <owusukevin17@gmail.com>
2022-06-09 23:39:49 +0100

    Readme message

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,7 +26,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 		if (mem == NULL)
 			return (NULL);
-		
+
 		return (mem);
 	}
 	
---
+++ README.md
@@ -0,0 +1,1 @@
+C programming language from Holberton-Alx SE
